7dd2092a24b484c050aad0f59b1e391e298b25a4
L <vasilegroza3@gmail.com>
2016-04-15 01:46:11 +0300

    add forgoten java class

---
+++ src/Usefull/FunctionG.java
@@ -0,0 +1,27 @@
+package Usefull;
+
+public class FunctionG {
+
+	private String function;
+	private String color;
+	private int stroke;
+	
+	public String getFunction() {
+		return function;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public int getStroke() {
+		return stroke;
+	}
+
+	public FunctionG(String function, String color, int stroke )
+	{
+		this.function=function;
+		this.color=color;
+		this.stroke=stroke;
+	}
+}
